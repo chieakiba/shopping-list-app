149c8f6309c2438b2bfe235a3e6d0125a4f2abb5
Chie <akiba.chie@gmail.com>
2016-05-04 15:52:08 -0700

    Hid 'Your list:' h3 until an item is entered. Can enter item and will append itself in the list

--- js/app.js
+++ js/app.js
@@ -1,26 +1,19 @@
 $(document).ready(function() {
-	//enable the "Add item" button to add items to list//
+	$('#addedItems').hide();
+	//enable the "Add item" button to add items to list and append new items to list//
 	$('button').click(function() {
-		($('input').val());
-	});
-	$('button').click(function() {
-		$('#To-buy').html($('input:text').val());
-		//append new items to list//
+		$('#addedItems').show();
 		var value = $('input:text').val();
-		$('#buy').append("<li>" + value + "</li>");
+		if (value != "") {
+			$('#buy').append("<li>" + value + "</li>");
+			$('input:text').val("");
+		}
+		else {
+			alert("Please enter an item");
+		}
 	});
 	//once clicked the checkmark, move the item to the Bought section
-	// $('button').click(function() {
-	// 	$('#To-buy').html($('input:text').val());
-	// 	//append new items to list//
-	// 	var value = $('input:text').val();
-	// 	$('#buy').append("<li>" + value + "</li>");
-	});
+	
 	//once clicked the x mark, move the item to the Deleted section
-	// $('button').click(function() {
-	// 	$('#To-buy').html($('input:text').val());
-	// 	//append new items to list//
-	// 	var value = $('input:text').val();
-	// 	$('#buy').append("<li>" + value + "</li>");
-	// });
-// });
+	
+});
