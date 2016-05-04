d1afdbb6e7d106640eb1d1cb30ab8fc03e133be9
Chie <akiba.chie@gmail.com>
2016-05-04 12:27:27 -0700

    Added jQuery code to append items. Also added section to move items to the Bought and Deleted section but code not enabled yet

--- js/app.js
+++ js/app.js
@@ -5,11 +5,22 @@ $(document).ready(function() {
 	});
 	$('button').click(function() {
 		$('#To-buy').html($('input:text').val());
+		//append new items to list//
+		var value = $('input:text').val();
+		$('#buy').append("<li>" + value + "</li>");
 	});
-	//append new items to list//
+	//once clicked the checkmark, move the item to the Bought section
 	$('button').click(function() {
-		var listItem = newItemOnTheList($('input:text').val());
-		$('#To-buy').append(listItem);
-		$('input:text').val("");
+		$('#To-buy').html($('input:text').val());
+		//append new items to list//
+		var value = $('input:text').val();
+		$('#buy').append("<li>" + value + "</li>");
+	});
+	//once clicked the x mark, move the item to the Deleted section
+	$('button').click(function() {
+		$('#To-buy').html($('input:text').val());
+		//append new items to list//
+		var value = $('input:text').val();
+		$('#buy').append("<li>" + value + "</li>");
 	});
 });
