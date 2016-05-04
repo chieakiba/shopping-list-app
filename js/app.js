46e47976dd9c0d728da0b2f3fb62ff97ca361d89
Chie <akiba.chie@gmail.com>
2016-05-04 11:28:42 -0700

    Added js folder, jQuery file and jQuery min file. Also enabled 'Add item' button to add items to list

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	//enable the "Add item" button to add items to list//
+	$('button').click(function() {
+		($('input').val());
+	});
+	$('button').click(function() {
+		$('#To-buy').html($('input:text').val());
+	});
+});
