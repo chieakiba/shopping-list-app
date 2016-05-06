48374b190d7b32592935f28ebe71d7dc3bca9f7d
Chie <akiba.chie@gmail.com>
2016-05-05 17:16:14 -0700

    Bought item and deleted item works and now only the items clicked into will move to each list

--- js/app.js
+++ js/app.js
@@ -8,17 +8,21 @@ $(document).ready(function() {
         var value = $('input:text').val();
         if (value != "") {
             $('#addedItems').show();
-            var item = "<li class='item' data-item-name='" + value + "'><a class='checkButton' href='#'><span class='check'></span></a>" + value + "<a class='checkButton' href='#'><span class='cross'></span></a></li>"
+            var item = "<li class='item' data-item-name='" + value + "'><a class='checkButton' href='#'><span class='check'></span></a>" + value + "<a class='crossButton' href='#'><span class='cross'></span></a></li>"
             $('#buy').append(item);
             $('input:text').val("");
             //once clicked the checkmark, move the item to the Bought section ("<li>" + item + "</li>")
-            $('.checkButton').click(function() {
+            $('.check').click(function() {
                 $('#boughtItems').show();
-                var itemBought = $('.item').clone();
+                var itemBought = $(this).parent().parent().remove();
                 $(itemBought).appendTo('#bought');
             });
             //once clicked the x mark, move the item to the Deleted section
-            
+            $('.cross').click(function() {
+                $('#deletedItems').show();
+                var itemBought = $(this).parent().parent().remove();
+                $(itemBought).appendTo('#removed');
+            });
         } else {
             alert("Please enter an item");
         }
